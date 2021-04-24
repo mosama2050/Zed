0de7e41ab74ff69f755b8ca70f1b26ede8f497f9
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-04-24 16:16:50 +0200

    create models

---
+++ Back_End/src/test/java/com/example/zed/ZedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
