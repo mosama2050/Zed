4cef4a86dbbfab1126adbc045ce732f83d9cfe78
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-04-26 00:43:12 +0200

    Implement API to Manage Comments

---
+++ Back_End/src/main/java/com/example/zed/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.example.zed.mapper;
+
+import com.example.zed.dto.CommentsDto;
+import com.example.zed.model.Comment;
+import com.example.zed.model.Post;
+import com.example.zed.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    @Mapping(target = "user", source = "user")
+    Comment map(CommentsDto commentsDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentsDto mapToDto(Comment comment);
+}
---
+++ Back_End/src/main/java/com/example/zed/service/CommentService.java
@@ -0,0 +1,62 @@
+package com.example.zed.service;
+
+import com.example.zed.dto.CommentsDto;
+import com.example.zed.exception.PostNotFoundException;
+import com.example.zed.mapper.CommentMapper;
+import com.example.zed.model.Comment;
+import com.example.zed.model.NotificationEmail;
+import com.example.zed.model.Post;
+import com.example.zed.model.User;
+import com.example.zed.repository.CommentRepository;
+import com.example.zed.repository.PostRepository;
+import com.example.zed.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+    private static final String POST_URL = "";
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
+
+    public void save(CommentsDto commentsDto) {
+        Post post = postRepository.findById(commentsDto.getPostId())
+                .orElseThrow(() -> new PostNotFoundException(commentsDto.getPostId().toString()));
+        Comment comment = commentMapper.map(commentsDto, post, authService.getCurrentUser());
+        commentRepository.save(comment);
+
+        String message = mailContentBuilder.build(authService.getCurrentUser() + " posted a comment on your post." + POST_URL);
+        sendCommentNotification(message, post.getUser());
+    }
+
+    private void sendCommentNotification(String message, User user) {
+        mailService.sendMail(new NotificationEmail(user.getUsername() + " Commented on your post", user.getEmail(), message));
+    }
+
+    public List<CommentsDto> getAllCommentsForPost(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException(postId.toString()));
+        return commentRepository.findByPost(post)
+                .stream()
+                .map(commentMapper::mapToDto).collect(toList());
+    }
+
+    public List<CommentsDto> getAllCommentsForUser(String userName) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException(userName));
+        return commentRepository.findAllByUser(user)
+                .stream()
+                .map(commentMapper::mapToDto)
+                .collect(toList());
+    }
+}
